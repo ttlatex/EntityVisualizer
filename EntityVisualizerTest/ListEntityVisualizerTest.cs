24983ed003b6767e5d6d56a7e356769e85a05d61
TOKISHIRAZU <ttlatex@yahoo.co.jp>
2017-05-14 17:47:09 +0900

    データ変更

--- EntityVisualizerTest/ListEntityVisualizerTest.cs
+++ EntityVisualizerTest/ListEntityVisualizerTest.cs
@@ -17,8 +17,14 @@ namespace EntityVisualizerTest
         public void TestShowVisualizer()
         {
             var entity = new List<IceCream> {
-                new IceCream { Flavor = "Vanilla", Price = 6 },
-                new IceCream { Flavor = "Choco", Price = 10 },
+                new IceCream { ID="1", Flavor = "Vanilla", Price = 6,Type = 0 },
+                new IceCream { ID="2", Flavor = "Choco", Price = 10,Type = 0 },
+                new IceCream { ID="3", Flavor = "Lemon", Price = 10,Type = 0 },
+                new IceCream { ID="4", Flavor = "Cookie", Price = 10,Type = 0 },
+                new IceCream { ID="5", Flavor = "Strawberry", Price = 10,Type = 0 },
+                new IceCream { ID="6", Flavor = "GreenTea", Price = 10,Type = 0 },
+                new IceCream { ID="7", Flavor = "Banana", Price = 10,Type = 0 },
+                new IceCream { ID="8", Flavor = "Melon", Price = 10,Type = 0 },
             };
 
             new VisualizerDevelopmentHost(entity, typeof(ListEntityVisualizer))
