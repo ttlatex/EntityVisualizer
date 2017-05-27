68066365e16c6aea42040ddd38301789a5d62cc8
TOKISHIRAZU <ttlatex@yahoo.co.jp>
2017-05-27 18:48:48 +0900

    テストデータ変更

--- EntityVisualizerTest/Entity/IceCream.cs
+++ EntityVisualizerTest/Entity/IceCream.cs
@@ -2,12 +2,11 @@
 
 namespace EntityVisualizerTest.Entity
 {
-    [Serializable]
     public class IceCream
     {
         public string ID { get; set; }
         public string Flavor { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
         public int Type { get; set; }
     }
 }
--- EntityVisualizerTest/ListEntityVisualizerTest.cs
+++ EntityVisualizerTest/ListEntityVisualizerTest.cs
@@ -13,12 +13,12 @@ namespace EntityVisualizerTest
         public void TestShowVisualizer()
         {
             var entity = new List<IceCream> {
-                new IceCream { ID="1", Flavor = "Vanilla", Price = 6,Type = 0 },
+                new IceCream { ID="1", Flavor = "Vanilla", Price = 6.3,Type = 0 },
                 new IceCream { ID="2", Flavor = "Choco", Price = 10,Type = 0 },
                 new IceCream { ID="3", Flavor = "Lemon", Price = 10,Type = 0 },
-                new IceCream { ID="4", Flavor = "Cookie", Price = 10,Type = 0 },
+                new IceCream { ID="4", Flavor = "Cookie", Price = 10.5,Type = 0 },
                 new IceCream { ID="5", Flavor = "Strawberry", Price = 10,Type = 0 },
-                new IceCream { ID="6", Flavor = "GreenTea", Price = 10,Type = 0 },
+                new IceCream { ID="6", Flavor = "GreenTea", Price = 0.3,Type = 0 },
                 new IceCream { ID="7", Flavor = "Banana", Price = 10,Type = 0 },
                 new IceCream { ID="8", Flavor = "Melon", Price = 10,Type = 0 },
             };
